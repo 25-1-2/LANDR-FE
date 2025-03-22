20f02552938496c49199f3e9ff57d9af4e3c7b77
zunione <xt.wish1@gmail.com>
2025-03-22 16:45:40 +0900

    INIT(presentation/build.gradle.kts, libs.versions.toml) : 프레젠테이션에 파이어베이스 라이브러리 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.hilt)
+    implementation(libs.firebase.auth.ktx)
     kapt(libs.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
