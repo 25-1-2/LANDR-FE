5edcc1359c1495580ba025d24ce4c42a92b918a4
zunione <xt.wish1@gmail.com>
2025-03-23 01:48:19 +0900

    INIT(ALL) : firebase 기본 및 authentication 설정 완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,13 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Android Gradle Plugin 등 필요한 classpath 설정
+    }
+}
---
+++ presentation/src/main/java/com/capston/presentation/ui/LoginScreen.kt
@@ -0,0 +1,2 @@
+package com.capston.presentation.ui
+
