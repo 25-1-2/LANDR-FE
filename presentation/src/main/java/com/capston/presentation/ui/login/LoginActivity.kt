933731638d6937fbf01af9e775e5655f3a6fea18
zunione <xt.wish1@gmail.com>
2025-04-04 21:02:12 +0900

    FEAT(LoginActivity) : viewModel을 통해 API에 회원가입 정보 전달

--- presentation/src/main/java/com/capston/presentation/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/capston/presentation/ui/login/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.Credential
 import androidx.credentials.CredentialManager
@@ -14,9 +15,11 @@ import androidx.credentials.GetCredentialRequest
 import androidx.credentials.exceptions.ClearCredentialException
 import androidx.credentials.exceptions.GetCredentialException
 import androidx.lifecycle.lifecycleScope
+import com.capston.domain.request.LoginDto
 import com.capston.presentation.R
 import com.capston.presentation.theme.CapstonTheme
 import com.capston.presentation.ui.MainActivity
+import com.capston.presentation.viewmodel.LoginViewModel
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.firebase.auth.FirebaseAuth
@@ -29,6 +32,8 @@ import kotlinx.coroutines.launch
 
 class LoginActivity : ComponentActivity() {
     private lateinit var auth: FirebaseAuth
+
+    val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,7 +76,7 @@ class LoginActivity : ComponentActivity() {
             .setServerClientId(getString(R.string.google_client_id))
             // true: Only show accounts previously used to sign in.
             .setFilterByAuthorizedAccounts(false)
-            .setAutoSelectEnabled(true)
+            .setAutoSelectEnabled(false)
             .build()
 
         // 2) CredentialRequest 생성
@@ -113,6 +118,13 @@ class LoginActivity : ComponentActivity() {
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d("LoginActivity", "signInWithCredential:success")
+                    // 백엔드에 회원가입 정보 전달
+                    loginViewModel.postLogin(
+                        LoginDto(
+                            auth.currentUser?.email,
+                            auth.currentUser?.displayName,
+                            "")
+                    )
                     // 로그인 성공 후 MainActivity로 이동
                     startActivity(Intent(this, MainActivity::class.java))
                     finish()
