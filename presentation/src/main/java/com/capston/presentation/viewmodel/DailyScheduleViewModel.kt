4f1ddc8a318814a1fb47122089f25044b3e06e9f
eccho03 <choeunchae0828@gmail.com>
2025-04-24 18:18:28 +0900

    FEAT(VIEWMODEL) : 모든 뷰모델에 loading indicator 추가

--- presentation/src/main/java/com/capston/presentation/viewmodel/DailyScheduleViewModel.kt
+++ presentation/src/main/java/com/capston/presentation/viewmodel/DailyScheduleViewModel.kt
@@ -3,6 +3,7 @@ package com.capston.presentation.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.capston.domain.manager.LoadingStateManager
 import com.capston.domain.response.daily_schedule.DailyScheduleResponse
 import com.capston.domain.response.home.DistinctHomeIdResponse
 import com.capston.domain.usecase.daily_schedule.GetDailyScheduleUseCase
@@ -14,12 +15,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DailyScheduleViewModel @Inject constructor(
-    private val getDailyScheduleUseCase: GetDailyScheduleUseCase
+    private val getDailyScheduleUseCase: GetDailyScheduleUseCase,
+    private val loadingStateManager: LoadingStateManager
 ) : ViewModel() {
     private val _getDailySchedule = MutableStateFlow(DailyScheduleResponse())
     val getDailySchedule: StateFlow<DailyScheduleResponse> = _getDailySchedule
 
     fun getDailySchedule(date: String) {
+        loadingStateManager.show()
         viewModelScope.launch {
             try {
                 getDailyScheduleUseCase(date).collect{ response ->
@@ -27,6 +30,8 @@ class DailyScheduleViewModel @Inject constructor(
                 }
             } catch (e: Exception) {
                 Log.e("getDailySchedule", e.toString())
+            } finally {
+                loadingStateManager.hide()
             }
         }
     }
