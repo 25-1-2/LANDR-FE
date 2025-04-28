422c940db6208d98d08a2d3a29d3ebe0da4d2d98
zunione <xt.wish1@gmail.com>
2025-04-28 20:33:43 +0900

    FEAT(LectureDetailScreen): 서버에서 불러온 데이터를 사용하도록 변경

--- presentation/src/main/java/com/capston/presentation/ui/LectureDetailScreen.kt
+++ presentation/src/main/java/com/capston/presentation/ui/LectureDetailScreen.kt
@@ -1,15 +1,14 @@
 package com.capston.presentation.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,70 +17,74 @@ import com.capston.presentation.theme.CapstonTheme
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import com.capston.domain.model.Lecture
+import com.capston.domain.model.LessonSchedule
+import com.capston.domain.response.plan.GetPlanDetailResponse
 
 
 @Composable
-fun LectureDetailScreen(lecture: Lecture) {
+fun LectureDetailScreen(planDetailResponse: GetPlanDetailResponse) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Row {
-            // 상단 제목
-            Text(
-                text = "2026 현우진의 수분감 - 수학(공통)",
-                style = MaterialTheme.typography.headlineSmall,
-                fontWeight = FontWeight.Bold,
-//                modifier = Modifier.padding(bottom = 16.dp)
-            )
-            // SVG 아이콘 추가 (벡터 에셋을 사용)
-            Icon(
-                imageVector = ImageVector.vectorResource(id = R.drawable.icon_reschedule),
-                contentDescription = "아이콘",
-                modifier = Modifier.minimumInteractiveComponentSize()
-            )
-
-        }
-
+        TitleSection(planDetailResponse = planDetailResponse)
 
         // 날짜별 섹션
-        DateSection(
-            date = "2022년 8월 20일",
-            tasks = listOf("함수의 극한과 연속 ①", "함수의 극한과 연속 ②", "함수의 극한과 연속 ③")
+        planDetailResponse.dailySchedules.forEach { schedule ->
+            OneDaySection(
+                date = schedule.date,
+                lessonSchedules = schedule.lessonSchedules
+            )
+        }
+    }
+}
+
+@Composable
+fun TitleSection(planDetailResponse: GetPlanDetailResponse) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 32.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalAlignment = Alignment.Top
+    ) {
+        Text(
+            text = planDetailResponse.lectureTitle,
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.weight(10f),
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        DateSection(
-            date = "2022년 8월 21일",
-            tasks = listOf("함수의 극한과 연속 ①", "함수의 극한과 연속 ②", "함수의 극한과 연속 ③")
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        DateSection(
-            date = "2022년 8월 22일",
-            tasks = listOf("함수의 극한과 연속 ①", "함수의 극한과 연속 ②", "함수의 극한과 연속 ③")
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.icon_reschedule),
+            contentDescription = "일정 변경",
+            modifier = Modifier
+                .clickable { /*…*/ }
         )
     }
 }
 
 @Composable
-fun DateSection(date: String, tasks: List<String>) {
-    Column {
+fun OneDaySection(date: String, lessonSchedules: List<LessonSchedule>) {
+    Column(
+        modifier = Modifier.padding(start = 8.dp)
+    ) {
         Text(
             text = date,
-            style = MaterialTheme.typography.titleMedium,
+            style = MaterialTheme.typography.titleLarge,
             fontWeight = FontWeight.Medium,
             modifier = Modifier.padding(bottom = 8.dp)
         )
-        tasks.forEach { task ->
-            TaskItem(taskText = task)
+        lessonSchedules.forEach { lessonSchedule ->
+            TaskItem(lessonSchedule = lessonSchedule)
         }
     }
 }
 
 @Composable
-fun TaskItem(taskText: String) {
+fun TaskItem(lessonSchedule: LessonSchedule) {
     // 각 체크박스의 상태를 기억합니다.
-    var checked by remember { mutableStateOf(false) }
+    var checked by remember { mutableStateOf(lessonSchedule.completed) }
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
@@ -94,7 +97,7 @@ fun TaskItem(taskText: String) {
         )
         Spacer(modifier = Modifier.width(8.dp))
         Text(
-            text = taskText,
+            text = lessonSchedule.lessonTitle,
             style = MaterialTheme.typography.bodySmall
         )
     }
@@ -104,6 +107,6 @@ fun TaskItem(taskText: String) {
 @Composable
 fun DetailScreenPreview() {
     CapstonTheme {
-        LectureDetailScreen(Lecture())
+        LectureDetailScreen(GetPlanDetailResponse())
     }
 }
