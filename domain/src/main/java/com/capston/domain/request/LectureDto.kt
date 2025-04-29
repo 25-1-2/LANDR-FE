eb2e4864935cfd3adf57beb117e23426c7885586
eccho03 <choeunchae0828@gmail.com>
2025-04-29 10:48:52 +0900

    FIX(Lecture) : cursorAt nullable 방지

--- domain/src/main/java/com/capston/domain/request/LectureDto.kt
+++ domain/src/main/java/com/capston/domain/request/LectureDto.kt
@@ -3,6 +3,6 @@ package com.capston.domain.request
 data class LectureDto (
     val search: String = "",
     val cursorLectureId: String = "",
-    val cursorCreatedAt: String = "",
+    val cursorCreatedAt: String? = "",
     val offset: String = "10",
 )
