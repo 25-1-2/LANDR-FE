6736bd1003ea6bf76ab52eb4d6aae650075e5c48
eccho03 <choeunchae0828@gmail.com>
2025-05-15 00:16:31 +0900

    CHORE(LoadingIndicator.kt) : 로딩 인디케이터 lottie로 변경

--- presentation/src/main/java/com/capston/presentation/ui/SearchActivity.kt
+++ presentation/src/main/java/com/capston/presentation/ui/SearchActivity.kt
@@ -6,15 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.navigation.compose.rememberNavController
+import com.capston.domain.manager.LoadingStateManager
 import com.capston.presentation.theme.CapstonTheme
 import com.capston.presentation.viewmodel.LectureViewModel
 import com.capston.presentation.viewmodel.PlanViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SearchActivity : ComponentActivity() {
     val lectureViewModel: LectureViewModel by viewModels()
     val planViewModel: PlanViewModel by viewModels()
+
+    @Inject
+    lateinit var loadingStateManager: LoadingStateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +29,7 @@ class SearchActivity : ComponentActivity() {
             val navController = rememberNavController()
 
             CapstonTheme {
-                SearchNavHost(navController, planViewModel, lectureViewModel)
+                SearchNavHost(navController, planViewModel, lectureViewModel, loadingStateManager)
             }
         }
     }
