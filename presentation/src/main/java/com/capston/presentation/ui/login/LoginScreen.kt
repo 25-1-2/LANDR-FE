0007da1e8952f50039fb510461fb624f561305d5
eccho03 <choeunchae0828@gmail.com>
2025-05-19 17:10:21 +0900

    FEAT(LoginScreen.kt) : 로그인 화면 ui 개발

--- presentation/src/main/java/com/capston/presentation/ui/login/LoginScreen.kt
+++ presentation/src/main/java/com/capston/presentation/ui/login/LoginScreen.kt
@@ -1,11 +1,26 @@
 package com.capston.presentation.ui.login
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.capston.presentation.R
+import com.capston.presentation.theme.textGray
 
 @Composable
 fun LoginScreen(
@@ -18,13 +33,36 @@ fun LoginScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("로그인 화면", style = MaterialTheme.typography.headlineMedium)
+
+        Image(
+            painter = painterResource(R.drawable.activity_splash_icon_iv),
+            contentDescription = "앱 로고",
+            modifier = Modifier
+                .size(130.dp)
+                .padding(start = 20.dp)
+        )
+
         Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            onClick = { onLoginButtonClick() },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("구글 로그인")
-        }
+
+        Image(
+            painter = painterResource(R.drawable.landr_title_iv),
+            contentDescription = "앱 타이틀",
+            modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)
+        )
+        Text(
+            text = "효과적인 인강 학습, LANDR와 함께\nLearn & Run",
+            style = MaterialTheme.typography.bodyMedium,
+            color = textGray,
+            textAlign = TextAlign.Center
+        )
+
+        Image(
+            painter = painterResource(R.drawable.screen_login_google_iv),
+            contentDescription = "로그인",
+            modifier = Modifier.clickable {
+                onLoginButtonClick()
+            }
+                .size(200.dp),
+        )
     }
 }
