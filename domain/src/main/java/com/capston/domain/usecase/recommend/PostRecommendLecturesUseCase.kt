17a5a62e6ec357ec0e774695bf6cd0b6498d5b01
eccho03 <choeunchae0828@gmail.com>
2025-06-04 12:58:50 +0900

    FEAT(RecommendApi) : 추천 api usecase 작성

---
+++ domain/src/main/java/com/capston/domain/usecase/recommend/PostRecommendLecturesUseCase.kt
@@ -0,0 +1,19 @@
+package com.capston.domain.usecase.recommend
+
+import com.capston.domain.repository.PlanRepository
+import com.capston.domain.repository.RecommendRepository
+import com.capston.domain.request.RecommendDto
+import com.capston.domain.response.MessageResponse
+import com.capston.domain.response.recommend.RecommendResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class PostRecommendLecturesUseCase @Inject constructor(
+    private val recommendRepository: RecommendRepository
+) {
+    operator fun invoke(recommendDto: RecommendDto): Flow<RecommendResponse> = flow {
+        val response = recommendRepository.postRecommendLectures(recommendDto)
+        emit(response)
+    }
+}
